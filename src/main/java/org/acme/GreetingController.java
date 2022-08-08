95c01ef00a348af60feede8dfb19a29d5b23df5b
Andrew Kelley <akelley@ttsiglobal.com>
2022-08-08 09:28:05 -0400

    Initial commit with quarkus spring  test application.

---
+++ src/main/java/org/acme/GreetingController.java
@@ -0,0 +1,28 @@
+package org.acme;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping("/{name}")
+    public Greeting hello(@PathVariable(name = "name") String name) {
+        return new Greeting("hello " + name);
+    }
+
+    public static class Greeting {
+        private final String message;
+
+        public Greeting(String message) {
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+    }
+}
